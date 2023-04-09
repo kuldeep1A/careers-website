71cef9f8107fdfae6be3ff8145df7da1a6a2dbf7
KULDEEP <90776144+kuldeep1A@users.noreply.github.com>
2023-04-09 11:56:42 +0000

    chnage

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_tamplate
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_k():
-    return render_tamplate("home.html")
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
