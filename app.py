1739137faf62b086988407419d92b874a1ad8221
KULDEEP <90776144+kuldeep1A@users.noreply.github.com>
2023-04-09 11:54:05 +0000

    add render_tamplate

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_tamplate
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_k():
-    return "<p>kjjklkj<p/>"
+    return render_tamplate("home.html")
 
 
 if __name__ == "__main__":
